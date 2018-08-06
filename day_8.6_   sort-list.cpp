80ae5922e6152dbf1a5f739005d65e063d9edad5
Xiayzn <36031022+Xiayzn@users.noreply.github.com>
2018-08-06 19:21:27 +0800

    Create day_8.6_   sort-list.cpp

---
+++ day_8.6_   sort-list.cpp
@@ -0,0 +1,24 @@
+[编程|20分] sort-list
+时间限制：C/C++ 1秒，其他语言 2秒
+空间限制：C/C++ 32768K，其他语言 65536K
+64bit IO Format: %lld
+本题可使用本地IDE编码，不做跳出限制，编码后请点击“保存并调试”按钮进行代码提交。
+题目描述
+
+Sort a linked list in O(n log n) time using constant space complexity.
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+        
+    }
+};
