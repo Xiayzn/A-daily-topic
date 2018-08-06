e0a67e6c0dd3de39fd948957ca2d832b4e8a00f8
Xiayzn <36031022+Xiayzn@users.noreply.github.com>
2018-08-06 19:20:50 +0800

    Create day_8.6_  reorder-list.cpp

---
+++ day_8.6_  reorder-list.cpp
@@ -0,0 +1,28 @@
+[编程|20分] reorder-list
+时间限制：C/C++ 1秒，其他语言 2秒
+空间限制：C/C++ 32768K，其他语言 65536K
+64bit IO Format: %lld
+本题可使用本地IDE编码，不做跳出限制，编码后请点击“保存并调试”按钮进行代码提交。
+题目描述
+
+Given a singly linked list L: L 0→L 1→…→L n-1→L n,
+reorder it to: L 0→L n →L 1→L n-1→L 2→L n-2→…
+You must do this in-place without altering the nodes' values.
+For example,
+Given{1,2,3,4}, reorder it to{1,4,2,3}.
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode *head) {
+        
+    }
+};
